56ae4e2feb11fd48d1441f66abdab4661a0cb839
Samuel Ajaru <samuelajaru@Samuels-MBP.attlocal.net>
2021-12-10 02:15:06 -0600

    branch7 commit

--- src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -2,6 +2,8 @@ package io.agileintelligence.ppmtool.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
+import io.agileintelligence.ppmtool.domain.Project;
 import io.agileintelligence.ppmtool.repositories.ProjectRepository;
 
 @Service
@@ -11,12 +13,13 @@ public class ProjectService {
 	private ProjectRepository projectRepository;
 	
 	public Project saveOrUpdateProject(Project project) {
-		
-		//Logic
-		
-		return projectRepository.save(project);
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		}catch (Exception e) {
+			throw new ProjectIdException("Project ID'"+project.getProjectIdentifier().toUpperCase()+"'already exists");
+		}
 		
 	}
 	
-
 }
