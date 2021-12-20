427a2c30dcc913bc1b51d70f1984ac883a1d5edc
Samuel Ajaru <samuelajaru@Samuels-MacBook-Pro.local>
2021-12-19 23:49:14 -0600

    branch19 commit

--- capstoneproject-react-client/src/components/Project/AddProject.js
+++ capstoneproject-react-client/src/components/Project/AddProject.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types"
+import {connect} from "react-redux"
+import {createProject} from "../../actions/projectActions";
 
 class AddProject extends Component {
   constructor() {
@@ -21,6 +24,7 @@ class AddProject extends Component {
   }
 
   onSubmit(e) {
+    e.preventDefault();
     const newProject = {
       projectName: this.state.projectName,
       projectIdentifier: this.state.projectIdentifier,
@@ -28,6 +32,7 @@ class AddProject extends Component {
       start_date: this.state.start_date,
       end_date: this.state.end_date,
     };
+    this.props.createProject(newProject, this.props.history)
   }
 
   render() {
@@ -116,4 +121,11 @@ class AddProject extends Component {
   }
 }
 
-export default AddProject;
+AddProject.propTypes = {
+    createProject = PropTypes.func.isRequired
+}
+
+export default connect(
+    null,
+    { createProject }
+    ) (AddProject);
