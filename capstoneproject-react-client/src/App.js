4c0d5db60018963322a0c26f34e08b5fce324792
Samuel Ajaru <samuelajaru@Samuels-MacBook-Pro.local>
2021-12-19 18:26:44 -0600

    branch16 commit

--- capstoneproject-react-client/src/App.js
+++ capstoneproject-react-client/src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 class App extends Component {
   render(){
   return (
+    <Router></Router>
     <div className="App">
       <Header />
-      <Dashboard />
+      <Route exact path="/dashboard" component={AddProject} />
+      <Route exact path="/addProject" component={AddProject} />
     </div>
+    </Router>
     );
   }
 }
